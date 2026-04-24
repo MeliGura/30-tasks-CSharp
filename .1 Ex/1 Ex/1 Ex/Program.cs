a6b9dcf871249e387de69414981f17a2b738b7bf
MeliGura <guramihail51@gmail.com>
2026-04-24 11:46:28 +0300

    start project

---
+++ .1 Ex/1 Ex/1 Ex/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int rows = 100; 
+        int cols = 200;
+
+        float[,] matrix = new float[rows, cols];
+        Random rand = new Random();
+        
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                matrix[i,j] = (float)(rand.NextDouble()*2 - 1);
+            }
+        }   
+        Console.WriteLine("Массив Заполнен");
+    }
+}
