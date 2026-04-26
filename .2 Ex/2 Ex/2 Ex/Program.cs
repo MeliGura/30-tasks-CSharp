d544fdff053b25e06a7d0d4ab4cdb506b398482f
MeliGura <guramihail51@gmail.com>
2026-04-26 17:43:49 +0300

    Task 2 completed

---
+++ .2 Ex/2 Ex/2 Ex/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int rows = 100;
+        int cols = 200;
+
+        int[,] matrix = new int[rows, cols];
+
+        Random rand = new Random();
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                matrix[i, j] = rand.Next(-3, 11);
+            }
+        }
+        Console.WriteLine("Массив Заполнен рандомными числами от -3 до 10");
+    }
+}
