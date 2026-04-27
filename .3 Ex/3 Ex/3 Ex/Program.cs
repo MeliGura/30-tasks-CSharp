6617415f4da455564b2e51f92cbe7e2fe00b6574
MeliGura <guramihail51@gmail.com>
2026-04-27 13:21:08 +0300

    Task 3

---
+++ .3 Ex/3 Ex/3 Ex/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Эпсилон
+        double eps = 1.0;
+        double prevEps = eps;
+
+        while (1.0 + eps != 1.0)
+        {
+            prevEps = eps;
+            eps = eps / 2.0;
+        }
+
+        Console.WriteLine("Машинный эпсилон: " + prevEps);
+
+
+        // Ноль
+        double x = 1.0;
+        double prevX = x;
+
+        while (x != 0.0)
+        {
+            prevX = x;
+            x = x / 2.0;
+        }
+
+        Console.WriteLine("Машинный ноль: " + prevX);
+    }
+}
