60aba5cb0a18f249c2c96c979550aa69a890277e
MeliGura <guramihail51@gmail.com>
2026-04-29 08:53:32 +0300

    Task 5

---
+++ .5 Ex/5 Ex/5 Ex/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static bool IsPrime(int num)
+    {
+        if (num < 2)
+            return false;
+
+        for (int i = 2; i < num; i++)
+        {
+            if (num % i == 0)
+                return false;
+        }
+
+        return true;
+    }
+    static void FindPrimes(int N)
+    {
+        for (int i = 2; i <= N; i++)
+        {
+            if (IsPrime(i))
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+
+    static void Main()
+    {
+        int N = 20;
+
+        Console.WriteLine("Простые числа до " + N + ":");
+        FindPrimes(N);
+
+        Console.WriteLine();
+        Test();
+    }
+
+    static void Test()
+    {
+        Console.WriteLine("Тест до 10:");
+        FindPrimes(10);
+    }
+}
