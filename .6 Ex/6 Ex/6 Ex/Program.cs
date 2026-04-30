1d78645b6f600d01170685b438120bb891d747a8
MeliGura <guramihail51@gmail.com>
2026-04-30 10:30:40 +0300

    Task 6

---
+++ .6 Ex/6 Ex/6 Ex/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+class Program
+{
+    static void ReverseArray(int[] arr)
+    {
+        int left = 0;
+        int right = arr.Length - 1;
+
+        while (left < right)
+        {
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+
+    static void Main()
+    {
+        int[] array = { 1, 2, 3, 4, 5 };
+
+        Console.WriteLine("До:");
+        PrintArray(array);
+
+        ReverseArray(array);
+
+        Console.WriteLine("После:");
+        PrintArray(array);
+
+        Test();
+    }
+
+    static void PrintArray(int[] arr)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.Write(arr[i] + " ");
+        }
+        Console.WriteLine();
+    }
+
+    static void Test()
+    {
+        int[] test = { 10, 20, 30 };
+
+        ReverseArray(test);
+
+        Console.WriteLine("Тест:");
+        PrintArray(test);
+    }
+}
