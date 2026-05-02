7d91cefd73ee2a1b50ddca81406b8ea53268dc98
MeliGura <guramihail51@gmail.com>
2026-05-02 17:01:34 +0300

    Task 7

---
+++ .7 Ex/7 Ex/7 Ex/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static bool IsPalindrome(string str)
+    {
+        int left = 0;
+        int right = str.Length - 1;
+
+        while (left < right)
+        {
+            if (str[left] != str[right])
+            {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    static void Main()
+    {
+        string text = "abba";
+
+        bool result = IsPalindrome(text);
+
+        Console.WriteLine("Результат: " + result);
+
+        Test();
+    }
+
+    static void Test()
+    {
+        Console.WriteLine(IsPalindrome("racecar")); 
+        Console.WriteLine(IsPalindrome("hello"));  
+    }
+}
