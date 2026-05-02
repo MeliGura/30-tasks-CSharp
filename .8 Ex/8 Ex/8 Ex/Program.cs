7a3202b41e2051b2e3435c7296c474970e26bc62
MeliGura <guramihail51@gmail.com>
2026-05-02 17:07:02 +0300

    Task 8

---
+++ .8 Ex/8 Ex/8 Ex/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static int FindMax(int[] arr)
+    {
+        int max = arr[0]; 
+
+        for (int i = 1; i < arr.Length; i++)
+        {
+            if (arr[i] > max)
+            {
+                max = arr[i];
+            }
+        }
+
+        return max;
+    }
+
+    static void Main()
+    {
+        int[] array = { 3, 7, 2, 9, 5 };
+
+        int result = FindMax(array);
+
+        Console.WriteLine("Максимум: " + result);
+
+        Test();
+    }
+
+    static void Test()
+    {
+        int[] test = { -1, -5, -3 };
+
+        Console.WriteLine("Тест: " + FindMax(test)); 
+    }
+}
