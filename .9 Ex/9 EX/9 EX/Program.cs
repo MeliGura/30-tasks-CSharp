4e0022f24b1240965a46ab3d97621a8734a01654
MeliGura <guramihail51@gmail.com>
2026-05-03 15:18:05 +0300

    Task 9

---
+++ .9 Ex/9 EX/9 EX/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static int CountEven(int[] arr)
+    {
+        int count = 0; 
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] % 2 == 0)
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    static void Main()
+    {
+        int[] array = { 1, 2, 3, 4, 6 };
+
+        int result = CountEven(array);
+
+        Console.WriteLine("Количество чётных: " + result);
+
+        Test();
+    }
+
+    static void Test()
+    {
+        int[] test = { 7, 9, 11 };
+
+        Console.WriteLine("Тест: " + CountEven(test));  
+    }
+}
