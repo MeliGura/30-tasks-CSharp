4baeb2e6f9e5fef9cac87cf9d939cdda73dd9f32
MeliGura <guramihail51@gmail.com>
2026-05-04 13:36:50 +0300

    Task 10

---
+++ .10 Ex/10 Ex/10 Ex/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static int FindMinIndex(int[] arr)
+    {
+        int min = arr[0];
+        int minIndex = 0;
+
+        for (int i = 1; i < arr.Length; i++)
+        {
+            if (arr[i] < min)
+            {
+                min = arr[i];
+                minIndex = i;
+            }
+        }
+
+        return minIndex;
+    }
+
+    static void Main()
+    {
+        int[] array = { 5, 2, 8, 1, 4 };
+
+        int index = FindMinIndex(array);
+
+        Console.WriteLine("Индекс минимума: " + index);
+
+        Test();
+    }
+
+    static void Test()
+    {
+        int[] test = { 10, 3, 7 };
+
+        Console.WriteLine("Тест: " + FindMinIndex(test)); 
+    }
+}
