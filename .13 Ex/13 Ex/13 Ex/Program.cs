2283911b850a0ab703a141556244204cbab6b6cf
MeliGura <guramihail51@gmail.com>
2026-05-05 19:58:56 +0300

    Task 13

---
+++ .13 Ex/13 Ex/13 Ex/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static bool Contains(int[] arr, int target)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] == target)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    static void Main()
+    {
+        int[] array = { 3, 7, 2, 9 };
+
+        bool result = Contains(array, 7);
+
+        Console.WriteLine("Найдено: " + result);
+
+        Test();
+    }
+
+    static void Test()
+    {
+        int[] test = { 1, 2, 3 };
+
+        Console.WriteLine(Contains(test, 2)); 
+        Console.WriteLine(Contains(test, 5)); 
+    }
+}
