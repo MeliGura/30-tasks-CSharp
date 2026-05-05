4c00ca147035a7dfba2589fdaff8da057cecc25b
MeliGura <guramihail51@gmail.com>
2026-05-05 19:44:30 +0300

    Task 12

---
+++ .12 Ex/12 Ex/12 Ex/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+class Program
+{
+    static void BubbleSort(int[] arr)
+    {
+        for (int i = 0; i < arr.Length - 1; i++)
+        {
+            for (int j = 0; j < arr.Length - 1 - i; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    static void Main()
+    {
+        int[] array = { 5, 2, 8, 1 };
+
+        Console.WriteLine("До:");
+        PrintArray(array);
+
+        BubbleSort(array);
+
+        Console.WriteLine("После:");
+        PrintArray(array);
+
+        Test();
+    }
+
+    static void PrintArray(int[] arr)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.Write(arr[i] + " ");
+        }
+        Console.WriteLine();
+    }
+
+    static void Test()
+    {
+        int[] test = { 3, 1, 2 };
+
+        BubbleSort(test);
+
+        Console.WriteLine("Тест:");
+        PrintArray(test);
+    }
+}
