948395e0bc94ef3b597936e1c7c75da262b95fe7
MeliGura <guramihail51@gmail.com>
2026-05-07 22:30:05 +0300

    Task 16

---
+++ .16 Ex/16 Ex/16 Ex/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static int CountVowels(string text)
+    {
+        int count = 0;
+
+        string vowels = "aeiouAEIOU";
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (vowels.Contains(text[i]))
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    static void Main()
+    {
+        string word = "Hello World";
+
+        int result = CountVowels(word);
+
+        Console.WriteLine("Количество гласных: " + result);
+
+        Test();
+    }
+
+    static void Test()
+    {
+        Console.WriteLine(CountVowels("cat")); 
+        Console.WriteLine(CountVowels("sky")); 
+    }
+}
