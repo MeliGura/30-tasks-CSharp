ffebe1913fbc07d3da625d17b9b354448b69e627
MeliGura <guramihail51@gmail.com>
2026-05-08 10:31:06 +0300

    Task 17

---
+++ .17 Ex/17 Ex/17 Ex/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+class Program
+{
+    static int MostFrequent(int[] arr)
+    {
+        int maxCount = 0;
+        int mostFrequent = arr[0];
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            int count = 0;
+
+            for (int j = 0; j < arr.Length; j++)
+            {
+                if (arr[i] == arr[j])
+                {
+                    count++;
+                }
+            }
+
+            if (count > maxCount)
+            {
+                maxCount = count;
+                mostFrequent = arr[i];
+            }
+        }
+
+        return mostFrequent;
+    }
+
+    static void Main()
+    {
+        int[] array = { 1, 2, 2, 3, 2, 4 };
+
+        int result = MostFrequent(array);
+
+        Console.WriteLine("Самое частое число: " + result);
+
+        Test();
+    }
+
+    static void Test()
+    {
+        int[] test = { 5, 5, 1, 1, 1 };
+
+        Console.WriteLine(MostFrequent(test)); 
+    }
+}
