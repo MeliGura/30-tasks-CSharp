1d4f635c67c0ba70366d11d9cafcf5616b50e188
MeliGura <guramihail51@gmail.com>
2026-05-09 11:17:04 +0300

    Task 18

---
+++ .18 Ex/18 Ex/18 Ex/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static bool IsSorted(int[] arr)
+    {
+        for (int i = 0; i < arr.Length - 1; i++)
+        {
+            if (arr[i] > arr[i + 1])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    static void Main()
+    {
+        int[] array = { 1, 2, 3, 4 };
+
+        bool result = IsSorted(array);
+
+        Console.WriteLine("Массив отсортирован: " + result);
+
+        Test();
+    }
+
+    static void Test()
+    {
+        int[] test1 = { 1, 2, 3 };
+        int[] test2 = { 5, 2, 7 };
+
+        Console.WriteLine(IsSorted(test1)); 
+        Console.WriteLine(IsSorted(test2)); 
+    }
+}
