2cae68b75487e056232046c6ae1f0c2eed1e15b0
MeliGura <guramihail51@gmail.com>
2026-05-09 13:35:54 +0300

    Task 19

---
+++ .19 Ex/19 Ex/19 Ex/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static int SumArray(int[] arr)
+    {
+        int sum = 0;
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            sum += arr[i];
+        }
+
+        return sum;
+    }
+
+    static void Main()
+    {
+        int[] array = { 1, 2, 3, 4 };
+
+        int result = SumArray(array);
+
+        Console.WriteLine("Сумма массива: " + result);
+
+        Test();
+    }
+
+    static void Test()
+    {
+        int[] test = { 5, 5, 5 };
+
+        Console.WriteLine(SumArray(test)); 
+    }
+}
