5d5ae80e2de55e65f01e6d5433cb27e8ab22566c
MeliGura <guramihail51@gmail.com>
2026-05-10 20:02:34 +0300

    Task 20

---
+++ .20 Ex/20 Ex/20 Ex/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static double Average(int[] arr)
+    {
+        int sum = 0;
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            sum += arr[i];
+        }
+
+        double avg = (double)sum / arr.Length;
+
+        return avg;
+    }
+
+    static void Main()
+    {
+        int[] array = { 2, 4, 6, 8 };
+
+        double result = Average(array);
+
+        Console.WriteLine("Среднее значение: " + result);
+
+        Test();
+    }
+
+    static void Test()
+    {
+        int[] test = { 1, 2, 3 };
+
+        Console.WriteLine(Average(test)); 
+    }
+}
