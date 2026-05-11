815c7b19f70d93626ec7ab5ff4af87fc799c6777
MeliGura <guramihail51@gmail.com>
2026-05-11 11:09:39 +0300

    Task 22

---
+++ .22 Ex/22 Ex/22 Ex/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Program
+{
+    static bool IsAnagram(string str1, string str2)
+    {
+        if (str1.Length != str2.Length)
+        {
+            return false;
+        }
+
+        char[] arr1 = str1.ToCharArray();
+        char[] arr2 = str2.ToCharArray();
+
+        Array.Sort(arr1);
+        Array.Sort(arr2);
+
+        for (int i = 0; i < arr1.Length; i++)
+        {
+            if (arr1[i] != arr2[i])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    static void Main()
+    {
+        string word1 = "listen";
+        string word2 = "silent";
+
+        bool result = IsAnagram(word1, word2);
+
+        Console.WriteLine("Анаграмма: " + result);
+
+        Test();
+    }
+
+    static void Test()
+    {
+        Console.WriteLine(IsAnagram("cat", "act")); 
+        Console.WriteLine(IsAnagram("cat", "dog")); 
+    }
+}
