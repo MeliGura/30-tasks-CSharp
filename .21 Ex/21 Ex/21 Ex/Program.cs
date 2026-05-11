c417be0b2632f87f04c85ac51c5bd1c64dc31e37
MeliGura <guramihail51@gmail.com>
2026-05-11 09:56:23 +0300

    Task 21

---
+++ .21 Ex/21 Ex/21 Ex/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+class Program
+{
+    static void SwapMinMax(int[] arr)
+    {
+        int minIndex = 0;
+        int maxIndex = 0;
+
+        for (int i = 1; i < arr.Length; i++)
+        {
+            if (arr[i] < arr[minIndex])
+            {
+                minIndex = i;
+            }
+
+            if (arr[i] > arr[maxIndex])
+            {
+                maxIndex = i;
+            }
+        }
+
+        int temp = arr[minIndex];
+        arr[minIndex] = arr[maxIndex];
+        arr[maxIndex] = temp;
+    }
+
+    static void Main()
+    {
+        int[] array = { 5, 2, 8, 1, 4 };
+
+        Console.WriteLine("До:");
+        PrintArray(array);
+
+        SwapMinMax(array);
+
+        Console.WriteLine("После:");
+        PrintArray(array);
+
+        Test();
+    }
+
+    static void PrintArray(int[] arr)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.Write(arr[i] + " ");
+        }
+
+        Console.WriteLine();
+    }
+
+    static void Test()
+    {
+        int[] test = { 10, 3, 7 };
+
+        SwapMinMax(test);
+
+        Console.WriteLine("Тест:");
+        PrintArray(test);
+    }
+}
