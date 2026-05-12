4ad69348d3a8d25b5ea73bf42a8e3abca9417b9d
MeliGura <guramihail51@gmail.com>
2026-05-12 09:54:37 +0300

    Task 24

---
+++ .24 Ex/24 Ex/24 Ex/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static int CountWords(string text)
+    {
+        string[] words = text.Split(' ');
+
+        return words.Length;
+    }
+
+    static void Main()
+    {
+        string text = "Я люблю программирование";
+
+        int result = CountWords(text);
+
+        Console.WriteLine(result);
+    }
+}
