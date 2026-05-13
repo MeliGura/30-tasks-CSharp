5273e45262bebb35ff6c01e259366ac3587e8332
MeliGura <guramihail51@gmail.com>
2026-05-13 09:51:48 +0300

    Task 26

---
+++ .26 Ex/26 Ex/26 Ex/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static int SecondMax(int[] arr)
+    {
+        int max = int.MinValue;
+        int secondMax = int.MinValue;
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] > max)
+            {
+                secondMax = max;
+                max = arr[i];
+            }
+            else if (arr[i] > secondMax && arr[i] != max)
+            {
+                secondMax = arr[i];
+            }
+        }
+
+        return secondMax;
+    }
+
+    static void Main()
+    {
+        int[] array = { 5, 8, 2, 10, 7 };
+
+        int result = SecondMax(array);
+
+        Console.WriteLine(result);
+    }
+}
