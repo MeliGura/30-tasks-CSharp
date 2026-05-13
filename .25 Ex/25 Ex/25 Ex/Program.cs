017e45cd06a6013007ce50e13d0dc40bde3f5b3d
MeliGura <guramihail51@gmail.com>
2026-05-13 09:37:01 +0300

    Task 25

---
+++ .25 Ex/25 Ex/25 Ex/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static bool IsPalindrome(int number)
+    {
+        int original = number;
+        int reversed = 0;
+
+        while (number > 0)
+        {
+            int digit = number % 10;
+
+            reversed = reversed * 10 + digit;
+
+            number = number / 10;
+        }
+
+        return original == reversed;
+    }
+
+    static void Main()
+    {
+        int number = 121;
+
+        bool result = IsPalindrome(number);
+
+        Console.WriteLine(result);
+    }
+}
