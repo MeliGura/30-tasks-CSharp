834b557a8b12aba1b5c405ea4d2367251ed9fc9c
MeliGura <guramihail51@gmail.com>
2026-05-15 06:41:33 +0300

    Task 28

---
+++ .28 Ex/28 Ex/28 Ex/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void ShiftRight(int[] arr)
+    {
+        int last = arr[arr.Length - 1];
+
+        for (int i = arr.Length - 1; i > 0; i--)
+        {
+            arr[i] = arr[i - 1];
+        }
+
+        arr[0] = last;
+    }
+
+    static void PrintArray(int[] arr)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.Write(arr[i] + " ");
+        }
+
+        Console.WriteLine();
+    }
+
+    static void Main()
+    {
+        int[] array = { 1, 2, 3, 4 };
+
+        ShiftRight(array);
+
+        PrintArray(array);
+    }
+}
