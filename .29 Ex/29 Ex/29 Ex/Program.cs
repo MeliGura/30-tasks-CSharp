e1f1003f379796d95fc93be6eb7a981ad5d32095
MeliGura <guramihail51@gmail.com>
2026-05-16 05:30:30 +0300

    Task 29

---
+++ .29 Ex/29 Ex/29 Ex/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static int CountVowels(string text)
+    {
+        int count = 0;
+
+        string vowels = "aeiouAEIOU";
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (vowels.Contains(text[i]))
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    static void Main()
+    {
+        string text = "programming";
+
+        int result = CountVowels(text);
+
+        Console.WriteLine(result);
+    }
+}
