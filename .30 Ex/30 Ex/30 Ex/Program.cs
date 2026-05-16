21842d4a2f4cc149834b8526de888e2cf7b032b0
MeliGura <guramihail51@gmail.com>
2026-05-16 06:37:25 +0300

    Task 30

---
+++ .30 Ex/30 Ex/30 Ex/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static bool AreEqual(string str1, string str2)
+    {
+        if (str1.Length != str2.Length)
+        {
+            return false;
+        }
+
+        for (int i = 0; i < str1.Length; i++)
+        {
+            if (str1[i] != str2[i])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    static void Main()
+    {
+        string first = "hello";
+        string second = "hello";
+
+        bool result = AreEqual(first, second);
+
+        Console.WriteLine(result);
+    }
+}
